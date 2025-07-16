fe969b2e98673e2673e6b9267921a32b2edabae8
RaunakMishr <mishraraunak431@gmail.com>
2025-07-16 19:32:44 +0530

    Header formatted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 const App = ()=>{
   return(
     <div>
-
+      <Routes>
+        <Route path='/' element ={<Home/>}></Route>
+        <Route path='/blog' element={<Blog/>}></Route>
+      </Routes>
     </div>
   )
 }
