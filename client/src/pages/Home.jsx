347302f9bcd3a27df9437586f79c68bd465bc06f
RaunakMishr <mishraraunak431@gmail.com>
2025-07-22 20:24:24 +0530

    Newsletter section updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import BlogList from '../components/BlogList';
+import Newsletter from '../components/Newsletter';
 const Home = ()=>{
     return(
         <>
             <Navbar/>
             <Header/>
             <BlogList/>
+            <Newsletter/>
         </>
     )
 }
