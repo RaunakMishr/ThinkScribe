2fde2a7818ba89ef2f3a6fe510098e6a332ce2e3
RaunakMishr <mishraraunak431@gmail.com>
2025-08-01 21:37:49 +0530

    Backend Server Created

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'dotenv/config'
+import cors from 'cors'
+
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())//all request will be parsed using json method
+
+//Routes
+app.get('/',(req, res)=>res.send("API is Working"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log('Server is running on port '+PORT)
+})
+
+export default app;
