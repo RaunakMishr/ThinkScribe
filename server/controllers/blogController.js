5ccd3d077983230936e7f8a121e0f578e50e53d9
RaunakMishr <mishraraunak431@gmail.com>
2025-08-02 13:27:59 +0530

    Blog controllers updated

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -7,8 +7,8 @@ export const addBlog = async(req, res)=>{
         const {title, subTitle, description, category, isPublished}= JSON.parse(req.body.blog);
         const imageFile = req.file;
 
-        //check if all fields are present !imageFile
-        if(!title || !description || !category){
+        //check if all fields are present
+        if(!title || !description || !category || !imageFile){
             return res.json({success:false, message:"Missing required fields"})
         }
         //we have to store image, we will store image url, so first we are storing it on cloud
@@ -39,3 +39,51 @@ export const addBlog = async(req, res)=>{
     }
 }
 
+
+export const getAllBlogs = async(req, res)=>{
+    try {
+        const blogs = await Blog.find({isPublished:true})
+        res.json({success:true, blogs})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
+
+export const getBlogById = async(req, res)=>{
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            return res.json({success:false, message:"Blog Not Found"})
+        }
+        res.json({success:true, blog})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+        
+    }
+}
+
+
+
+export const deleteBlogById = async(req, res)=>{
+    try {
+        const { id } = req.body;
+        await Blog.findByIdAndDelete(id);
+        res.json({success:true, message:"Blog deleted successfully!"})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+        
+    }
+}
+
+export const togglePublish = async(req, res)=>{
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished
+        await blog.save();
+        res.json({success:true, message:"Blog status updated!"})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
